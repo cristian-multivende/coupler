9baaf47df770f060498d00d428e2ac030d67ee0e
Cristian Parra <cristian@multivende.com>
2025-07-11 11:11:51 -0400

    First commit

---
+++ exito-1-create.test.js
@@ -0,0 +1,67 @@
+//@ts-check
+"use strict";
+import config from '../../server/config/environment';
+import mongoose from "mongoose";
+import i18n from 'i18n';
+import _ from "lodash";
+
+i18n.configure({
+  directory: __dirname + '/../../server/i18n',
+  prefix: 'locale_',
+  defaultLocale: 'es_CL'
+});
+
+const { ProductManagerAsync, ProductManagerAsyncHandler } = require("../../server/components/connect/marketplace/product-manager-async");
+
+class ProductManagerAsyncOnlyRead extends ProductManagerAsync {
+  constructor() {
+    super(new ProductManagerAsyncHandler());
+  }
+  async updateProductLinkStatus(processingSynchronizationStatus, productLinkLogEntryType, productLinkLogEntryCode, productLinkLogEntry, productLink) {
+    /** @type {any} */
+    let updates = {
+      synchronizationStatus: processingSynchronizationStatus,
+      externalContent: JSON.stringify({
+        type: productLinkLogEntryType,
+        code: productLinkLogEntryCode
+      }),
+      // @ts-ignore
+      LastProductLinkLogEntryId: _.toString(productLinkLogEntry?._id)
+    };
+    console.log(updates);
+  }
+}
+(async () => {
+  try {
+    let productLinks = [
+
+    ];
+
+    let masterSyncManager = require(`../../server/components/background-tasks/sync-manager/dequeue`);
+    const SyncManagerFactory = require("../../server/utils/sync-manager-factory");
+    const HookSyncManagerFactory = require("../../server/utils/hook-sync-manager-factory");
+    await SyncManagerFactory.initializeFactory(masterSyncManager);
+    await HookSyncManagerFactory.initializeFactory(masterSyncManager);
+    let productManagerAsyncOnlyRead = new ProductManagerAsyncOnlyRead();
+    const dbUrl = process.env.MONGO_URL || config.mongodb.uri;
+    let mongoConfig = {};
+    console.log("Attempt to connect");
+    await mongoose.connect(dbUrl, mongoConfig);
+
+    for (const productlink of productLinks) {
+      console.log("productlink:", productlink);
+      const task = {
+        MarketplaceConnectionId: "0f149a35-ff1b-4da6-a21d-3b2826caec65",
+        ProductLinkId: productlink,
+      };
+      await productManagerAsyncOnlyRead.productUpload(task);
+    }
+    console.log('Fin exito-1-create.test');
+  } catch (error) {
+    console.error("exito-1-create.test: Error occurred");
+    console.log(error.stack ?? error.message);
+  } finally {
+    await mongoose.disconnect();
+    process.exit(0);
+  }
+})();
